c93651cdca631ed05540aac7749cc682f61b5985
Moore <tmzlovenoelle@gmail.com>
2024-03-29 10:29:50 +0800

    添加了valid集

--- pj_1/BP_fitting_test.py
+++ pj_1/BP_fitting_test.py
@@ -8,20 +8,24 @@ from utils import *
 func = sin
 interval = (-math.pi, math.pi)
 train_num = 6000
+valid_num = 100
 test_num = 20
 noise = 0.01
 X_train, y_train = create_data(func=func, interval=interval, sample_num=train_num, noise=noise)
+X_valid, y_valid = create_data(func=func, interval=interval, sample_num=valid_num, noise=noise)
 X_test, y_test = create_data(func=func, interval=interval, sample_num=test_num, noise=noise)
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
+X_valid = np.array(X_valid)
+y_valid = np.array(y_valid)
 X_test = np.array(X_test)
 y_test = np.array(y_test)
 
 
 batch_size = 16
 layers = [1, 16, 32, 1]
-BP_net = back_propagation(layers=layers, batch_size=batch_size)
+BP_net = back_propagation(layers=layers, batch_size=batch_size, X_valid=X_valid, y_valid=y_valid)
 BP_net.train(X_train, y_train)
 y_pred = BP_net.predict(X_test)
 print("test error = ", mean_squared_error(y_pred, y_test))
@@ -32,7 +36,6 @@ x = np.linspace(-np.pi, np.pi, 300)  # 在 -π 到 π 的范围内生成 100 个
 # 计算对应的 y 值
 y = np.squeeze(BP_net.predict(x[:, np.newaxis]))
 
-vectorized_test = np.vectorize(test)
 
 X_train = np.sort(X_train, axis=0)
 y_train = np.sin(X_train)
